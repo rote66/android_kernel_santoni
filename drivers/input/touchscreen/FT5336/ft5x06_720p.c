5b22f267fe09d170c0c66100d63d924c6c352ba0
Ryan Andri <ryanandri@linuxmail.org>
2017-12-11 22:23:27 +0700

    input: fixed touchscreen.
    - full working by Noyer (https://github.com/bitrvmpd/msm-3.18).
    - all credits goes his work
    
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>

--- drivers/input/touchscreen/FT5336/ft5x06_720p.c
+++ drivers/input/touchscreen/FT5336/ft5x06_720p.c
@@ -2571,11 +2571,21 @@ static int ft5x06_ts_probe(struct i2c_client *client,
 
 	/* check the controller id */
 	reg_addr = FT_REG_ID;
-	err = ft5x06_i2c_read(client, &reg_addr, 1, &reg_value, 1);
-	if (err < 0) {
-		dev_err(&client->dev, "version read failed");
-		goto free_reset_gpio;
-	}
+	do { //Search for the correct register.
+		err = ft5x06_i2c_read(client, &reg_addr, 1, &reg_value, 1);
+		if (err < 0) {
+			dev_err(&client->dev, "version read failed");
+			//goto free_reset_gpio;
+		}
+
+		if(reg_value!=0x14){
+		 	client->addr = client->addr + 0x1;
+		} else {
+			dev_info(&client->dev, "Touchpanel Register found: 0x%x\n",client->addr);
+			break;
+		}
+	} while(reg_value!=0x14);//We expect it to be 0x14 for FT5336
+
 	ic_name = reg_value;
 
 	dev_info(&client->dev, "Device ID = 0x%x\n", reg_value);
